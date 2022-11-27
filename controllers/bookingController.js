030276aab4b0ec27e6acf5c1c1dcee660a84c92f
Carl D <carlthur@student.chalmers.se>
2022-11-27 21:47:27 +0100

    Refactored some of the functions

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,72 +2,81 @@ var BookingRequest = require("../models/bookingRequest.js");
 
 /*
 Create a new booking from the incomming mqtt message.
-mosquitto_pub -t 'request/availability/good' -m '{"email": "JO22msg@gmail.com","name": "Carl","clinicId": 1,"issuance": "1602406766314","date": "2020-12-14", "start": "0900", "end": "1000"}'
+mosquitto_pub -t 'request/availability/good' -m '{"email": "AM2Z@gmail.com","name": "Carl","clinicId": 1,"issuance": "1602406766314","date": "2020-12-14", "start": "0900", "end": "1000"}'
 */
-const createBooking = async (reservation) => {
-  try {
-    const { email, name, clinicId, issuance, date, state, start, end } =
-      reservation;
-    reservation = new BookingRequest({
-      user: {
-        email: email,
-        name: name,
-      },
-      clinicId: clinicId,
-      issuance: issuance,
-      date: date,
-      state: state,
-      start: start,
-      end: end,
-    });
+class ClinicBookingController {
+  createBooking = async (reservation) => {
+    try {
+      const { email, name, clinicId, issuance, date, state, start, end } =
+        reservation;
+      reservation = new BookingRequest({
+        user: {
+          email: email,
+          name: name,
+        },
+        clinicId: clinicId,
+        issuance: issuance,
+        date: date,
+        state: state,
+        start: start,
+        end: end,
+      });
 
-    //console.log('bookingrequest: ' + reservation)
-    reservation.save((err, res) => {
-      if (err) return console.log(err);
-      else return JSON.stringify(reservation);
-      // console.log(JSON.stringify(reservation))
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-/* 
+      //console.log('bookingrequest: ' + reservation)
+      reservation.save((err) => {
+        if (err) return console.log(err);
+        else return JSON.stringify(reservation);
+        // console.log(JSON.stringify(reservation))
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  /* 
 Function changes the state field to 'approved', it finds it by id.
-mosquitto_pub -t 'request/booking/approve' -m '{"_id": "637f992b4793782041e5648f"}'
+mosquitto_pub -t 'request/booking/approve' -m '{"_id": "6383cc2c39a711d9cf6772bb"}'
 */
-const approveBooking = async (request) => {
-  try {
-    const { _id } = request;
-    const filter = { _id: _id };
-    const update = { state: "approved" };
+  approveBooking = async (request) => {
+    try {
+      const { _id } = request;
 
-    const booking = await BookingRequest.findOneAndUpdate(filter, update);
-    console.log(booking);
+      BookingRequest.findById(_id, function (err, bookingRequest) {
+        if (err) {
+          return next(err);
+        }
+        if (!bookingRequest) {
+          return (res = { message: "Booking request was not found" });
+        }
+        bookingRequest.state = "approved";
+        bookingRequest.save();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-} catch (error) {
-    console.log(error);
-  }
-};
-
-/* 
+  /* 
 Function changes the state field to 'denied', it finds it by id.
-mosquitto_pub -t 'request/booking/denied' -m '{"_id": "637f992b4793782041e5648f"}'
+mosquitto_pub -t 'request/booking/denied' -m '{"_id": "6383cb76f1578e5e752b1444"}'
 */
 
-const denieBooking = async (request) => {
-  try {
-    const { _id } = request;
-    const filter = { _id: _id };
-    const update = { state: "denied" };
+  denieBooking = async (request, res) => {
+    try {
+      const { _id } = request;
 
-    const booking = await BookingRequest.findOneAndUpdate(filter, update);
-    console.log(booking);
-
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-module.exports = createBooking;
-module.exports = approveBooking;
-module.exports = denieBooking;
+      BookingRequest.findById(_id, function (err, bookingRequest) {
+        if (err) {
+          return next(err);
+        }
+        if (!bookingRequest) {
+          return (res = { message: "Booking request was not found" });
+        }
+        bookingRequest.state = "denied";
+        bookingRequest.save();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+}
+module.exports = ClinicBookingController;
--- controllers/mqtt-handler.js
+++ controllers/mqtt-handler.js
@@ -1,87 +1,91 @@
-const mqtt = require('mqtt');
-const createBooking = require('./bookingController');
-const approveBooking = require('./bookingController');
-const denieBooking = require('./bookingController');
+const mqtt = require("mqtt");
+const ClinicBookingController = require("./bookingController");
+const approveBooking = require("./bookingController");
+const denieBooking = require("./bookingController");
 
-
+const clinic = new ClinicBookingController();
 class MqttHandler {
-constructor() {
+  constructor() {
     this.mqttClient = null;
-    this.host = 'http://localhost:1883';
-    this.username = 'YOUR_USER'; // mqtt credentials if these are needed to connect
-    this.password = 'YOUR_PASSWORD';
+    this.host = "http://localhost:1883";
+    this.username = "YOUR_USER"; // mqtt credentials if these are needed to connect
+    this.password = "YOUR_PASSWORD";
 
-    this.bookingRequestTopic = 'request/availability/good';
-    this.sendConfirmation = 'response/booking/confirmed';
+    this.bookingRequestTopic = "request/availability/good";
+    this.sendConfirmation = "response/booking/confirmed";
 
-    this.AlterApproveBooking = 'request/booking/approve';
-    this.AlterBookingDenied = 'request/booking/denied';
-}
-/*
+    this.AlterApproveBooking = "request/booking/approve";
+    this.AlterBookingDenied = "request/booking/denied";
+  }
+  /*
 mosquitto_sub -v -t 'response/booking/confirmed'
 */
-connect() {
-  // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
-  try {
-    this.mqttClient = mqtt.connect(this.host, { username: this.username, password: this.password });
-  } catch (error) {
-    console.log(error);
-  }
+  connect() {
+    // Connect mqtt with credentials (in case of needed, otherwise we can omit 2nd param)
+    try {
+      this.mqttClient = mqtt.connect(this.host, {
+        username: this.username,
+        password: this.password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
 
-  const localMqttClient = this.mqttClient;
+    const localMqttClient = this.mqttClient;
 
-  // Mqtt error calback
-  this.mqttClient.on('error', (err) => {
-    console.log(err);
-    this.mqttClient.end();
-  });
+    // Mqtt error calback
+    this.mqttClient.on("error", (err) => {
+      console.log(err);
+      this.mqttClient.end();
+    });
 
-  // Connection callback
-  this.mqttClient.on('connect', () => {
-    console.log(`Mqtt Client connected, Subscribed to '${this.bookingRequestTopic}'`);
-    this.mqttClient.subscribe(this.bookingRequestTopic, {qos: 1});
-    this.mqttClient.subscribe(this.AlterApproveBooking, {qos: 1});
-    this.mqttClient.subscribe(this.AlterBookingDenied, {qos: 1});
+    // Connection callback
+    this.mqttClient.on("connect", () => {
+      console.log(
+        `Mqtt Client connected, Subscribed to '${this.bookingRequestTopic}'`
+      );
+      this.mqttClient.subscribe(this.bookingRequestTopic, { qos: 1 });
+      this.mqttClient.subscribe(this.AlterApproveBooking, { qos: 1 });
+      this.mqttClient.subscribe(this.AlterBookingDenied, { qos: 1 });
+    });
 
-  });
+    //TODO: Create function that checks if the payload of the mqtt is valid JSON format.
 
-
-  //TODO: Create function that checks if the payload of the mqtt is valid JSON format. 
-
-  // When a message arrives, console.log it
-    this.mqttClient.on('message', async function (topic, message) {
-
+    // When a message arrives, console.log it
+    this.mqttClient.on("message", async function (topic, message) {
       switch (topic) {
-
-        case 'request/availability/good':
-        const confirmation = await createBooking(JSON.parse(message.toString()));
-        localMqttClient.publish('response/booking/confirmed', confirmation);
-        console.log('Sent to Client: ' + confirmation);
-      break;
-
-        case 'request/booking/approve':
-        const response = await approveBooking(JSON.parse(message.toString()));
-        //localMqttClient.publish('response/booking/approve', response);
-        console.log(response)
+        case "request/availability/good":
+          const confirmation = await clinic.createBooking(
+            JSON.parse(message.toString())
+          );
+          localMqttClient.publish("response/booking/confirmed", confirmation);
+          console.log("Sent to Client: " + confirmation);
           break;
 
-        case 'request/booking/denied':
-        const bookingResponse = await denieBooking(JSON.parse(message.toString()));
-        //localMqttClient.publish('response/booking/denied', bookingResponse);
-        console.log(bookingResponse)
-        break;
+        case "request/booking/approve":
+          const response = await clinic.approveBooking(
+            JSON.parse(message.toString())
+          );
+          //localMqttClient.publish('response/booking/approve', response);
+          console.log(response);
+          break;
 
-
-        }
-      });
-    }
-/*
+        case "request/booking/denied":
+          const bookingResponse = await clinic.denieBooking(
+            JSON.parse(message.toString())
+          );
+          //localMqttClient.publish('response/booking/denied', bookingResponse);
+          console.log(bookingResponse);
+          break;
+      }
+    });
+  }
+  /*
   // Sends a mqtt message to topic: mytopic
   sendMessage(message) {
     this.mqttClient.publish(this.sendConfirmation, message);
   }
   */
 }
-
 
 module.exports = MqttHandler;
